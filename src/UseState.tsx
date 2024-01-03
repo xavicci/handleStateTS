4b5b5ce5a688b0d96c720d86aa5e117154de2817
Xavi Flores <xa_franc@live.com>
2024-01-02 21:27:53 -0600

    estado declarativos

--- src/UseState.tsx
+++ src/UseState.tsx
@@ -3,55 +3,124 @@ import { useEffect, useState } from "react";
 type Props = {
   name: string;
 };
+
+type CompState = {
+  value: string;
+  error: boolean;
+  loading: boolean;
+  deleted: boolean;
+  confirmed: boolean;
+};
+
 const SECURITY_CODE = "asd";
 
 export const UseState = ({ name }: Props) => {
-  const [error, setError] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [value, setValue] = useState<string>("");
+  const [state, setState] = useState<CompState>({
+    value: "",
+    error: false,
+    loading: false,
+    deleted: false,
+    confirmed: false,
+  });
 
   useEffect(() => {
     console.log("start the effect");
+    console.log(state);
 
-    if (loading) {
+    if (state.loading) {
       setTimeout(() => {
         console.log("start Validated");
 
-        if (value !== SECURITY_CODE) {
-          setError(true);
+        if (state.value === SECURITY_CODE) {
+          setState({
+            ...state,
+            error: false,
+            loading: false,
+            confirmed: true,
+          });
+        } else {
+          setState({
+            ...state,
+            error: true,
+            loading: false,
+          });
         }
-        setLoading(false);
 
         console.log("end validated");
       }, 1000);
     }
     console.log("end Effect");
-  }, [loading, value]);
+  }, [state]);
 
-  return (
-    <div>
-      <h2>Delete {name}</h2>
-      <p>Por favor, escribe code para erase</p>
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Delete {name}</h2>
+        <p>Por favor, escribe code para erase</p>
+        {state.error && !state.loading && <p>Error: el code es incorrect</p>}
+        {state.loading && <p>loading..</p>}
+        <input
+          placeholder="code de security"
+          value={state.value}
+          onChange={(e) => {
+            setState({ ...state, value: e.target.value });
+          }}
+        />
 
-      {error && <p>Error: el code es incorrect</p>}
-      {loading && <p>loading..</p>}
-      <input
-        placeholder="code de security"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-
-      <button
-        onClick={() => {
-          setLoading(true);
-          setError(false);
-        }}
-      >
-        CheckIn
-      </button>
-      <button onClick={() => setError(!error)}>CheckInError</button>
-    </div>
-  );
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              loading: true,
+            });
+          }}
+        >
+          CheckIn
+        </button>
+      </div>
+    );
+  } else if (state.confirmed && !state.deleted) {
+    return (
+      <>
+        <p>Are you sure to delete?</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              deleted: true,
+            });
+          }}
+        >
+          Yes
+        </button>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+            });
+          }}
+        >
+          No
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>Delete Completed</p>
+        <button
+          onClick={() => {
+            setState({
+              ...state,
+              confirmed: false,
+              deleted: false,
+            });
+          }}
+        >
+          Reset, Come Back
+        </button>
+      </>
+    );
+  }
 };
